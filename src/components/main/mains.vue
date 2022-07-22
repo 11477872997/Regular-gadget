dd9edd48156a31e622de8828711b7e28c2db288a
11477872997 <977139863@qq.com>
2022-07-22 18:03:56 +0800

    更新类型

--- src/components/main/mains.vue
+++ src/components/main/mains.vue
@@ -1,10 +1,19 @@
 <template>
-    <el-row class="mb-4">
-    <el-button type="primary">身份证</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
+    <el-row :gutter="20">
+    <el-button type="primary">校验身份证</el-button>
+    <el-button type="success">校验手机号码</el-button>
+    <el-button type="info">校验中文</el-button>
+    <el-button type="warning">校验Email地址</el-button>
+    <el-button type="danger">校验域名</el-button>
+    <el-button type="primary">校验电话号码</el-button>
+    <el-button type="success">校验国内电话号码</el-button>
+    <el-button type="info">校验日期格式</el-button>
+    <el-button type="warning">校验xml文件</el-button>
+    <el-button type="danger">中国邮政编码</el-button>
+    <el-button type="primary">校验IP-v4地址</el-button>
+    <el-button type="primary">校验IP-v6地址</el-button>
+    <el-button type="success">校验金额2位小数</el-button>
+    <el-button type="success">判断IE的版本</el-button>
   </el-row>
 </template>
     
