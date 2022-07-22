73779eed658a1dfee7b646a75844ce41d3e586fe
11477872997 <977139863@qq.com>
2022-07-22 16:16:16 +0800

    初始化

---
+++ src/components/header/headers.vue
@@ -0,0 +1,26 @@
+<template>
+       <el-carousel :interval="4000" type="card" height="300px" @change="changePandT" loop> 
+              <el-carousel-item v-for="item in arr" :key="item" >
+                <a :href="item.url" target="_blank"><img :src="item.src" :alt="item.url"  /></a>
+              </el-carousel-item>
+       </el-carousel>
+</template>
+    
+<script setup lang='ts'>
+//  x渲染数据
+    let arr: any [] = [
+        {id:'1',src:new URL('../../assets/a.png', import.meta.url).href,url:'https://www.cnblogs.com/zxh-bug'},
+        {id:'2',src:'https://www.dmoe.cc/random.php',url:'https://www.cnblogs.com/zxh-bug'},
+        {id:'3',src:'https://acg.toubiec.cn/random.php'+new Date(),url:'https://www.cnblogs.com/zxh-bug'}
+       ];
+function changePandT(index :any) {
+       (index == 0) ?arr[index].src = new URL('../../assets/a.png', import.meta.url).href:arr[index].src = 'https://www.dmoe.cc/random.php?time='+new Date();  
+}
+</script>
+    
+<style scoped>
+img{
+  width: 80%;
+  height: 100%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import headers from '@/components/header/headers.vue'
+import mains from '@/components/main/mains.vue'
+</script>
+
+<template>
+   <div class="common-layout">
+      <el-container>
+         <el-header>
+            <headers />
+         </el-header>
+         <el-main>
+            <mains />
+         </el-main>
+         <el-footer>
+           
+         </el-footer>
+      </el-container>
+   </div>
+</template>
+<style scoped>
+.el-header {
+   height: auto;
+}
+</style>
