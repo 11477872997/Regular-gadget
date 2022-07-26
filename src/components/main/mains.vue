6520040aec1bb7946ff456c1a0c00cb9ca2b310c
11477872997 <977139863@qq.com>
2022-07-26 17:49:02 +0800

    更新数据

--- src/components/main/mains.vue
+++ src/components/main/mains.vue
@@ -2,25 +2,9 @@
  <div class="demo-collapse">
     <el-collapse v-model="activeNames" @change="handleChange">
       <el-collapse-item :title="titleName" name="1">
-      <el-button type="primary">校验身份证</el-button>
-       <!-- <el-row :gutter="20">
-          <el-button type="primary">校验身份证</el-button>
-          <el-button type="success">校验手机号码</el-button>
-          <el-button type="info">校验中文</el-button>
-          <el-button type="warning">校验Email地址</el-button>
-          <el-button type="danger">校验域名</el-button>
-          <el-button type="primary">校验电话号码</el-button>
-          <el-button type="success">校验国内电话号码</el-button>
-          <el-button type="info">校验日期格式</el-button>
-          <el-button type="warning">校验xml文件</el-button>
-          <el-button type="danger">中国邮政编码</el-button>
+        <el-row :gutter="20">
+
         </el-row>
-       <el-row :gutter="20">
-          <el-button type="primary">校验IP-v4地址</el-button>
-          <el-button type="primary">校验IP-v6地址</el-button>
-          <el-button type="success">校验金额2位小数</el-button>
-          <el-button type="success">判断IE的版本</el-button>
-        </el-row> -->
       </el-collapse-item>
     </el-collapse>
   </div>
@@ -34,13 +18,14 @@
 class randomNUm{
   arrlist: Array<string> = ["primary","success","info","warning","danger"];
   getrandom(){
-    return Math.random() * 5;
+    return this.arrlist[Math.round(Math.random()* 4)]
   }
 }
 let roum =  new randomNUm();
-console.log(roum.getrandom())
-console.log(parseInt('1'))
- const activeNames = ref(['1'])
+// 获取按钮数据
+
+
+const activeNames = ref(['1'])
 const handleChange = (val: string[]) => {
   console.log(val)
 }
