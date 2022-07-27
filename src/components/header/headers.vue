ee16651de3595eaf9545d1945450ca2163aeb738
11477872997 <977139863@qq.com>
2022-07-27 17:30:55 +0800

    更新样式的

--- src/components/header/headers.vue
+++ src/components/header/headers.vue
@@ -1,6 +1,6 @@
 <template>
        <el-carousel :interval="4000" type="card" height="300px" @change="changePandT" loop> 
-              <el-carousel-item v-for="item in arr" :key="item" >
+              <el-carousel-item v-for="item in arr" :key="item.id" >
                 <a :href="item.url" target="_blank"><img :src="item.src" :alt="item.url"  /></a>
               </el-carousel-item>
        </el-carousel>
@@ -11,7 +11,7 @@
     let arr: any [] = [
         {id:'1',src:new URL('../../assets/a.png', import.meta.url).href,url:'https://www.cnblogs.com/zxh-bug'},
         {id:'2',src:'https://www.dmoe.cc/random.php',url:'https://www.cnblogs.com/zxh-bug'},
-        {id:'3',src:'https://acg.toubiec.cn/random.php'+new Date(),url:'https://www.cnblogs.com/zxh-bug'}
+        {id:'3',src:'https://www.dmoe.cc/random.php',url:'https://www.cnblogs.com/zxh-bug'}
        ];
 function changePandT(index :any) {
        (index == 0) ?arr[index].src = new URL('../../assets/a.png', import.meta.url).href:arr[index].src = 'https://www.dmoe.cc/random.php?time='+new Date();  
@@ -20,7 +20,7 @@ function changePandT(index :any) {
     
 <style scoped>
 img{
-  width: 80%;
-  height: 100%;
+  width: 70%;
+  height: 95%;
 }
 </style>
--- src/components/main/mains.vue
+++ src/components/main/mains.vue
@@ -1,38 +1,73 @@
 <template>
  <div class="demo-collapse">
-    <el-collapse v-model="activeNames" @change="handleChange">
-      <el-collapse-item :title="titleName" name="1">
-        <el-row :gutter="20">
-
-        </el-row>
+    <el-collapse  accordion>
+      <el-collapse-item title="数字效验正则" name="1" >
+        <template v-for="item in users" >
+               <el-button  :color="roum.getrandom()"  v-if="item.name ==='figure'" :key="item.id" @click.stop="onThat(item.refular)">{{item.text}}</el-button>
+            </template>
+      </el-collapse-item>
+      <el-collapse-item title="字符效验正则" name="2">
+          <template v-for="item in users" >
+              <el-button :color="roum.getrandom()"  v-if="item.name ==='character'" :key="item.id" @click.stop="onThat(item.refular)">{{item.text}}</el-button>
+            </template>
+      </el-collapse-item>
+      <el-collapse-item title="特殊需求正则" name="3">
+          <template v-for="item in users" >
+              <el-button :color="roum.getrandom()"  v-if="item.name ==='special'" :key="item.id" @click.stop="onThat(item.refular)">{{item.text}}</el-button>
+            </template>
       </el-collapse-item>
     </el-collapse>
+    <el-row :gutter="20">
+        <el-col :span="12"><div class="grid-content ep-bg-purple" />
+             <el-input v-model="regularValue" placeholder="选择上面提供的正则" clearable />
+        </el-col>
+        <el-col :span="6"><div class="grid-content ep-bg-purple" />
+            <el-input v-model="contentValue" placeholder="输入要匹配的内容是否正确" clearable /></el-col>
+        <el-col :span="6"><div class="grid-content ep-bg-purple" />
+           <el-button type="primary" @click.stop="result">匹配结果</el-button>
+        </el-col>
+      </el-row>
   </div>
-   
 </template>
-    
 <script setup lang='ts'>
- import { ref } from 'vue'
- const titleName = '常用的正则'
-//  生成随机类型
-class randomNUm{
-  arrlist: Array<string> = ["primary","success","info","warning","danger"];
-  getrandom(){
-    return this.arrlist[Math.round(Math.random()* 4)]
-  }
+ import {ref } from 'vue'
+ // 正则输入框
+let regularValue = ref();
+let contentValue = ref();
+    //  生成随机类型
+    class randomNUm{
+      getrandom(){
+         let color = "#";
+         for (let i = 0; i < 6; i++){
+          let num:any = Math.random() * 16;
+           color += parseInt(num).toString(16);
+         } 
+        return color;
+      }
+    
+    }
+    let roum =  new randomNUm();
+    // 获取按钮数据
+    const users = ref();
+    import('../../assets/btn.json').then((res) =>{
+      users.value = res.default;
+    })
+// 获取当前正则
+ const onThat = (refular:string)=>{
+    regularValue.value = refular;
+  } 
+// 匹配结果
+const result = () =>{
+  regularValue.value.test(contentValue.value)
+    console.log(regularValue.value)
+    console.log(contentValue.value)
+    console.log( regularValue.value.test(contentValue.value))
 }
-let roum =  new randomNUm();
-// 获取按钮数据
 
-
-const activeNames = ref(['1'])
-const handleChange = (val: string[]) => {
-  console.log(val)
-}
 </script>
     
 <style scoped>
-.el-row {
-  margin-bottom: 20px;
+.el-button,.el-row{
+  margin: 10px;
 }
 </style>
